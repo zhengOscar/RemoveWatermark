7f3e472ca27f2aed01fcd45ef0be9c5575cf4a0a
ZhengOscar <540632763@qq.com>
2024-06-28 19:50:28 +0800

    调整结构

--- wxcloudrun/datas/menu.py
+++ wxcloudrun/datas/menu.py
@@ -4,21 +4,21 @@ data = [
     {
         'name':u'基础知识',
         'children':[
-            {'name':u'字母','alias':'abc','color':'#F1EBFE'},
-            {'name':u'数字','alias':'digit','color':'#E4F6FF'},
-            {'name':u'颜色','alias':'color','color':'#E7FFEA'},
-            {'name':u'形状','alias':'shape','color':'#FEF3EB'},
+            {'name':u'字母','alias':'abc','icon':'abc','color':'#F1EBFE'},
+            {'name':u'数字','alias':'digit','icon':'digit','color':'#E4F6FF'},
+            {'name':u'颜色','alias':'color','icon':'color','color':'#E7FFEA'},
+            {'name':u'形状','alias':'shape','icon':'shape','color':'#FEF3EB'},
         ]
     },
     {
         'name':u'进阶知识',
         'children':[
-            {'name':u'水果','alias':'fruit','color':'#F1EBFE'},
-            {'name':u'蔬菜','alias':'vegetable','color':'#E4F6FF'},
-            {'name':u'人物','alias':'character','color':'#E7FFEA'},
-            {'name':u'动物','alias':'animal','color':'#FEF3EB'},
-            {'name':u'日常用品','alias':'daily','color':'#E7FFEA'},
-            {'name':u'交通工具','alias':'traffic','color':'#FEF3EB'},
+            {'name':u'水果','alias':'fruit','icon':'fruit','color':'#F1EBFE'},
+            {'name':u'蔬菜','alias':'vegetable','icon':'vegetable','color':'#E4F6FF'},
+            {'name':u'人物','alias':'character','icon':'character','color':'#E7FFEA'},
+            {'name':u'动物','alias':'animal','icon':'animal','color':'#FEF3EB'},
+            {'name':u'日常用品','alias':'daily','icon':'daily','color':'#E7FFEA'},
+            {'name':u'交通工具','alias':'traffic','icon':'traffic','color':'#FEF3EB'},
         ]
     }
 ]
